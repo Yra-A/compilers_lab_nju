75b914e08d7de52914136ecf36e7571f089550db
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-12-24 15:20:13 +0800

    > make secpl24003 yra Darwin Yra 22.2.0 Darwin Kernel Version 22.2.0: Fri Nov 11 02:03:51 PST 2022; root:xnu-8792.61.2~4/RELEASE_ARM64_T6000 arm64 15:20  up 1 day,  4:13, 2 users, load averages: 14.65 11.63 10.41 c09becbc62ac5c4457e78f52c51868caedda4039

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
-{    
-    public static void main(String[] args){}
+{
+    public static void main(String[] args){
+        if (args.length != 1) {
+            System.err.println("can't find the command!");
+            System.exit(1);
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error when reading the file: " + e.getMessage());
+            System.exit(2);
+        }
+    }
 }
