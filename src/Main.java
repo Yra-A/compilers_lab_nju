a36e5000075f519a92064584e4d16fe7f748b0cf
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-12-27 00:02:40 +0800

    > make secpl24003 yra Darwin Yra 22.2.0 Darwin Kernel Version 22.2.0: Fri Nov 11 02:03:51 PST 2022; root:xnu-8792.61.2~4/RELEASE_ARM64_T6000 arm64  0:02  up  5:39, 4 users, load averages: 11.28 10.28 10.05 df7da1cdf2335d55d40f7d28a86500902afebd

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,37 @@
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("input path is required");
         }
         String source = args[0];
         CharStream input = CharStreams.fromFileName(source);
-        SysYLexer sysYLexer = new SysYLexer(input);
-        sysYLexer.removeErrorListeners();
-        myErrorListener myErrorListener = new myErrorListener();
-        sysYLexer.addErrorListener(myErrorListener);
-        List<Token> tokens = (List<Token>) sysYLexer.getAllTokens();
 
-        // 如果出现错误就不输出token
-        if (myErrorListener.hasError()) {
-            return;
-        }
-        for (Token token : tokens) {
-            String text = token.getText();
-            if (token.getType() == SysYLexer.INTEGER_CONST) {
-                if (text.length() > 1) {
-                    if (text.startsWith("0x") || text.startsWith("0X")) {
-                        if (text.length() == 2) {
-                            System.err.printf("%s %s at Line %d.\n", SysYLexer.VOCABULARY.getSymbolicName(SysYLexer.INTEGER_CONST), text.charAt(0), token.getLine());
-                            System.err.printf("%s %s at Line %d.\n", SysYLexer.VOCABULARY.getSymbolicName(SysYLexer.IDENT), text.charAt(1), token.getLine());
-                            continue;
-                        }
-                        // 十六进制
-                        text = text.substring(2);
-                        text = String.valueOf(Integer.parseInt(text, 16));
-                    } else if (text.startsWith("0")) {
-                        // 八进制
-                        text = text.substring(1);
-                        text = String.valueOf(Integer.parseInt(text, 8));
-                    }
-                }
-            }
-            String typeStr = SysYLexer.VOCABULARY.getSymbolicName(token.getType());
-            if (token.getType() == SysYLexer.LINE_COMMENT || token.getType() == SysYLexer.WS || token.getType() == SysYLexer.MULTILINE_COMMENT) {
-                continue;
-            }
-            System.err.printf("%s %s at Line %d.\n", typeStr, text, token.getLine());
+        SysYLexer sysYLexer = new SysYLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
+
+        SysYParser sysYParser = new SysYParser(tokens);
+        sysYParser.removeErrorListeners();
+        myErrorListener myErrorListener = new myErrorListener();
+        sysYParser.addErrorListener(myErrorListener);
+
+        ParseTree tree = sysYParser.program();
+        if (!myErrorListener.hasError()) {
+            Visitor visitor = new Visitor(sysYLexer);
+            visitor.visit(tree);
         }
     }
 }
 
-class myErrorListener extends BaseErrorListener
-{
+class myErrorListener extends BaseErrorListener {
     private boolean hasError = false;
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
@@ -63,5 +43,65 @@ class myErrorListener extends BaseErrorListener
 
     public boolean hasError() {
         return hasError;
+    }
+}
+
+class Visitor extends SysYParserBaseVisitor<Void> {
+
+    private String[] highlightColors;
+
+    public Visitor(SysYLexer sysYLexer) {
+        super();
+        getHighlightColor(sysYLexer);
+    }
+
+    @Override
+    public Void visitChildren(RuleNode node) {
+        String ruleName = SysYParser.ruleNames[node.getRuleContext().getRuleIndex()];
+        System.err.println(ruleName); // 打印正在访问的规则名称
+        return null;
+    }
+
+    @Override
+    public Void visitTerminal(TerminalNode node) {
+        Token token = node.getSymbol();
+        if (token != null) {
+            int type = token.getType();
+            String text = token.getText();
+            System.err.printf("[%s]\n", highlightColors[type]);
+        }
+        return null;
+    }
+
+    private Void getHighlightColor(SysYLexer sysYLexer) {
+        highlightColors = new String[sysYLexer.getVocabulary().getMaxTokenType() + 1];
+        //保留字
+        highlightColors[SysYLexer.CONST] = "orange";
+        highlightColors[SysYLexer.INT] = "orange";
+        highlightColors[SysYLexer.VOID] = "orange";
+        highlightColors[SysYLexer.IF] = "orange";
+        highlightColors[SysYLexer.ELSE] = "orange";
+        highlightColors[SysYLexer.WHILE] = "orange";
+        highlightColors[SysYLexer.BREAK] = "orange";
+        highlightColors[SysYLexer.CONTINUE] = "orange";
+        highlightColors[SysYLexer.RETURN] = "orange";
+        highlightColors[SysYLexer.PLUS] = "blue";
+        highlightColors[SysYLexer.MINUS] = "blue";
+        highlightColors[SysYLexer.MUL] = "blue";
+        highlightColors[SysYLexer.DIV] = "blue";
+        highlightColors[SysYLexer.MOD] = "blue";
+        highlightColors[SysYLexer.ASSIGN] = "blue";
+        highlightColors[SysYLexer.EQ] = "blue";
+        highlightColors[SysYLexer.NEQ] = "blue";
+        highlightColors[SysYLexer.LT] = "blue";
+        highlightColors[SysYLexer.GT] = "blue";
+        highlightColors[SysYLexer.LE] = "blue";
+        highlightColors[SysYLexer.GE] = "blue";
+        highlightColors[SysYLexer.NOT] = "blue";
+        highlightColors[SysYLexer.AND] = "blue";
+        highlightColors[SysYLexer.OR] = "blue";
+        highlightColors[SysYLexer.IDENT] = "red";
+        highlightColors[SysYLexer.INTEGER_CONST] = "green";
+        return null;
     }
 }
