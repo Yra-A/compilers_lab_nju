90669db1a03d03cfa69352f9d8f883d2649f7de9
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-12-27 00:34:28 +0800

    > make secpl24003 yra Darwin Yra 22.2.0 Darwin Kernel Version 22.2.0: Fri Nov 11 02:03:51 PST 2022; root:xnu-8792.61.2~4/RELEASE_ARM64_T6000 arm64  0:34  up  6:11, 4 users, load averages: 13.13 12.37 12.47 bb2073c6ccefbadc51ea6838767f57d21ce422d6

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         sysYParser.removeErrorListeners();
         myErrorListener myErrorListener = new myErrorListener();
         sysYParser.addErrorListener(myErrorListener);
-        System.out.println(sysYLexer.getAllTokens().size() + "??????????");
+        System.err.println(sysYLexer.getAllTokens().size() + "??????????");
         ParseTree tree = sysYParser.program();
         if (myErrorListener.hasError()) {
             Visitor visitor = new Visitor(sysYLexer);
