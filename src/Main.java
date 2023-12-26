cb6aa3b7958212fad3c2af44faa4d8dd7fa127ad
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-12-27 00:24:23 +0800

    > make secpl24003 yra Darwin Yra 22.2.0 Darwin Kernel Version 22.2.0: Fri Nov 11 02:03:51 PST 2022; root:xnu-8792.61.2~4/RELEASE_ARM64_T6000 arm64  0:24  up  6:01, 4 users, load averages: 17.38 13.38 12.71 888ab47c133e1880ce0e84816ab900a9d6be9f2d

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main {
         }
         String source = args[0];
         CharStream input = CharStreams.fromFileName(source);
-        System.err.println(input.toString());
         SysYLexer sysYLexer = new SysYLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
         SysYParser sysYParser = new SysYParser(tokens);
