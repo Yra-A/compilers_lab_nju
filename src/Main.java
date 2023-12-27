a28b0ef80274c18446fdccf4dc33c18927e578ca
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-12-27 18:29:39 +0800

    > make secpl24003 yra Darwin Yra 22.2.0 Darwin Kernel Version 22.2.0: Fri Nov 11 02:03:51 PST 2022; root:xnu-8792.61.2~4/RELEASE_ARM64_T6000 arm64 18:29  up 1 day, 6 mins, 5 users, load averages: 14.35 9.25 8.33 53be73daa29ec3a3e1771c7f4e35feca56312630

--- src/Main.java
+++ src/Main.java
@@ -1,133 +1,154 @@
+//lab2 code
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-public class Main {
+import java.util.BitSet;
+
+
+public class Main{
+    public static String [] ruleNames;
+    public static String [] lexerRuleNames;
+
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("input path is required");
+            return;
         }
         String source = args[0];
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
+
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
         SysYParser sysYParser = new SysYParser(tokens);
 
         sysYParser.removeErrorListeners();
-        MyErrorParserListener myErrorParserListener = new MyErrorParserListener();
-        sysYParser.addErrorListener(myErrorParserListener);
+        MyErrorListener myErrorListener = new MyErrorListener();
+        sysYParser.addErrorListener(myErrorListener);
+
 
         ParseTree tree = sysYParser.program();
-        if (!myErrorParserListener.hasError()) {
-            Visitor visitor = new Visitor(sysYLexer);
-            visitor.visit(tree);
+        Visitor visitor = new Visitor();
+
+        Main.ruleNames = sysYParser.getRuleNames();
+
+        Main.lexerRuleNames = sysYLexer.getRuleNames();
+
+        if(!myErrorListener.output){
+            visitor.visit(tree);//TODO
         }
-    }
-}
 
-class MyErrorParserListener extends BaseErrorListener {
-    private boolean hasError = false;
-    @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
-            String msg, RecognitionException e)
-    {
-        hasError = true;
-        System.err.printf("Error type B at Line %d:%s\n", line, msg);
-    }
-
-    public boolean hasError() {
-        return hasError;
-    }
-}
+    }// close main
+}// close class
 
 class Visitor extends SysYParserBaseVisitor<Void> {
-
-    private String[] highlightColors;
-    private int indent = 0;
-
-    private String[] tokenNames;
-
-    public Visitor(SysYLexer sysYLexer) {
-        super();
-        getHighlightColor(sysYLexer);
-        tokenNames = sysYLexer.getRuleNames();
-    }
-
-    private Void printIndent() {
-        for (int i = 0; i < indent; i++) {
-            System.out.print(" ");
-        }
-        return null;
-    }
+    private int depth = 0;
 
     @Override
     public Void visitChildren(RuleNode node) {
-        String ruleName = SysYParser.ruleNames[node.getRuleContext().getRuleIndex()];
-        printIndent();
-        indent += 2;
-        ruleName = ruleName.substring(0, 1).toUpperCase() + ruleName.substring(1);
-        System.err.println(ruleName); // 打印正在访问的规则名称
+        //TODO
+        depth = node.getRuleContext().depth();
+        displayParser(Main.ruleNames[node.getRuleContext().getRuleIndex()], depth);
 
-        Void result = this.defaultResult();
-        int n = node.getChildCount();
-        for(int i = 0; i < n && this.shouldVisitNextChild(node, result); ++i) {
-            ParseTree c = node.getChild(i);
-            Void childResult = c.accept(this);
-            result = this.aggregateResult(result, childResult);
+        return super.visitChildren(node);
+    }
+
+    public void displayParser(String text, int depth){
+        for (int i = 1; i < depth; i++) {
+            System.err.print("  ");
         }
-        indent -= 2;
-        return result;
+        System.err.println(text.substring(0,1).toUpperCase() + text.substring(1));
     }
 
     @Override
     public Void visitTerminal(TerminalNode node) {
-        Token token = node.getSymbol();
-        if (token != null) {
-            int type = token.getType() - 1;
-            if (type < 0 || type >= highlightColors.length || highlightColors[type] == null) {
-                return null;
-            }
-            String text = token.getText();
-            printIndent();
-            System.err.printf("%s %s[%s]", text, SysYLexer.ruleNames[type], highlightColors[type]);
+        //TODO
+        int type = node.getSymbol().getType();
+        String word = "";
+        if(type != -1){
+            word = Main.lexerRuleNames[type-1];
         }
-        return null;
+
+        RuleContext ruleContext = new RuleContext((RuleContext) node.getParent(), 0);
+        depth = ruleContext.depth();
+        displayLexer(node.getSymbol().getText(), word, depth);
+
+        return super.visitTerminal(node);
     }
 
-    private Void getHighlightColor(SysYLexer sysYLexer) {
-        highlightColors = new String[sysYLexer.getVocabulary().getMaxTokenType()];
-        //保留字
-        highlightColors[SysYLexer.CONST - 1] = "orange";
-        highlightColors[SysYLexer.INT - 1] = "orange";
-        highlightColors[SysYLexer.VOID - 1] = "orange";
-        highlightColors[SysYLexer.IF - 1] = "orange";
-        highlightColors[SysYLexer.ELSE - 1] = "orange";
-        highlightColors[SysYLexer.WHILE - 1] = "orange";
-        highlightColors[SysYLexer.BREAK - 1] = "orange";
-        highlightColors[SysYLexer.CONTINUE - 1] = "orange";
-        highlightColors[SysYLexer.RETURN - 1] = "orange";
-        highlightColors[SysYLexer.PLUS - 1] = "blue";
-        highlightColors[SysYLexer.MINUS - 1] = "blue";
-        highlightColors[SysYLexer.MUL - 1] = "blue";
-        highlightColors[SysYLexer.DIV - 1] = "blue";
-        highlightColors[SysYLexer.MOD - 1] = "blue";
-        highlightColors[SysYLexer.ASSIGN - 1] = "blue";
-        highlightColors[SysYLexer.EQ - 1] = "blue";
-        highlightColors[SysYLexer.NEQ - 1] = "blue";
-        highlightColors[SysYLexer.LT - 1] = "blue";
-        highlightColors[SysYLexer.GT - 1] = "blue";
-        highlightColors[SysYLexer.LE - 1] = "blue";
-        highlightColors[SysYLexer.GE - 1] = "blue";
-        highlightColors[SysYLexer.NOT - 1] = "blue";
-        highlightColors[SysYLexer.AND - 1] = "blue";
-        highlightColors[SysYLexer.OR - 1] = "blue";
-        highlightColors[SysYLexer.IDENT - 1] = "red";
-        highlightColors[SysYLexer.INTEGER_CONST - 1] = "green";
-        return null;
+    public void displayLexer(String text, String word, int depth){
+        String [] reservedWords = {"const", "int", "void", "if", "else", "while", "break", "continue", "return"};
+        String [] operators = {"plus", "minus", "mul", "div", "mod", "assign", "eq", "neq", "lt", "gt", "le", "ge", "not", "and", "or"};
+        String [] excludedWords = {"L_PAREN", "R_PAREN", "L_BRACE", "R_BRACE", "L_BRACKT", "R_BRACKT", "SEMICOLON", "COMMA"};
+        if(text.equals("<EOF>")){
+            return;
+        }
+        for(String excludedWord:excludedWords){
+            if(word.equals(excludedWord))
+                return;
+        }
+        String color = "";
+        for (String reservedWord : reservedWords) {
+            if (word.toLowerCase().equals(reservedWord)) {
+                color = "[orange]";
+                break;
+            }
+        }
+        for (String operator : operators) {
+            if (word.toLowerCase().equals(operator)) {
+                color = "[blue]";
+                break;
+            }
+        }
+        if(word.equals("IDENT")){
+            color = "[red]";
+        }
+        if(word.equals("INTEGR_CONST")){
+            color = "[green]";
+        }
+
+        // 处理8进制,16进制
+        if (text.startsWith("0x") || text.startsWith("0X")){
+            text = "" + Integer.parseInt(text.substring(2),16);
+        } else if (text.startsWith("0") && text.length() > 1){
+            text = "" + Integer.parseInt(text.substring(1),8);
+        }
+
+        //output
+        for (int i = 1; i < depth; i++) {
+            System.err.print("  ");
+        }
+        System.err.println(text + " " + word + color);
+
+    }
+
+}
+
+
+class MyErrorListener implements ANTLRErrorListener{
+    public boolean output = false;
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        System.err.println("Error type B at Line " + line + ": msg");
+        output = true;
+    }
+
+    @Override
+    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+
+    }
+
+    @Override
+    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+
     }
 }
