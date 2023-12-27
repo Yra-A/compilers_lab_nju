cc2b60839568942a4a9933fdd5078932b977e7a7
Yra-A <1325083019@qq.com>
2023-12-27 12:13:17 +0800

    lab2 update

--- src/Main.java
+++ src/Main.java
@@ -60,7 +60,7 @@ class Visitor extends SysYParserBaseVisitor<Void> {
 
     private Void printIndent() {
         for (int i = 0; i < indent; i++) {
-            System.out.print("  ");
+            System.out.print(" ");
         }
         return null;
     }
